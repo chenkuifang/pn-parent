cdddd9996a5e5015fb085e1f49db9611d5f49d08
chenkuifang <314287696@qq.com>
2018-03-05 16:53:18 +0800

    增加配置文件管理服务

---
+++ pn-config-server/src/main/java/com/example/eureka/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**   
+* @author QuiFar   
+* @date 2018年1月30日 下午10:24:10 
+* @version V1.0   
+*/
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+}
