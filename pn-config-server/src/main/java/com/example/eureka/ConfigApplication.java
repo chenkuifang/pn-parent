a9814d8bec1037551be616da8c036426241a8248
chenkuifang <314287696@qq.com>
2018-03-05 21:20:10 +0800

    增加文件

--- pn-config-server/src/main/java/com/example/eureka/ConfigApplication.java
+++ pn-config-server/src/main/java/com/example/eureka/ConfigApplication.java
@@ -2,6 +2,7 @@ package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**   
 * @author QuiFar   
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 * @version V1.0   
 */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
 
 	public static void main(String[] args) {
---
+++ pn-config-server/src/main/java/com/example/eureka/IndexController.java
@@ -0,0 +1,19 @@
+package com.example.eureka;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: test
+ * @author QuiFar
+ * @date 2018年3月5日 下午8:51:42
+ * @version V1.0
+ */
+@RestController
+public class IndexController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
