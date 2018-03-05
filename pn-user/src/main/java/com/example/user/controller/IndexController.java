cde95348010f1d63762c12899c5d94e2b9e8cf21
chenkuifang <314287696@qq.com>
2018-03-05 16:55:39 +0800

    增加配置文件管理服务

--- pn-user/src/main/java/com/example/user/controller/IndexController.java
+++ pn-user/src/main/java/com/example/user/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
     @GetMapping("/index")
     public String index() {
         return "hello user";
+    }
+
+    @GetMapping("/get-services")
+    public String getServices() {
+        return discoveryClient.getServices().toString();
     }
 
 }
