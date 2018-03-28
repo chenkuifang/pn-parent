4ee9eec3f1f44cdce5e12704a73ed68cd9cabb52
chenkuifang <314287696@qq.com>
2018-03-28 17:07:24 +0800

    add

--- pn-order/src/main/resources/dataSql/order.sql
+++ pn-order/src/main/resources/dataSql/order.sql
@@ -10,20 +10,28 @@ USE db_order;
 -- ----------------------------
 -- Table structure for db_order
 -- ----------------------------
+-- 订单信息表 --
 DROP TABLE IF EXISTS `pn_order`;
 CREATE TABLE `pn_order` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单编码',
+  `order_sid` varchar(40) NOT NULL COMMENT '订单号',
+
+  `pay_sid` varchar(40) NOT NULL COMMENT '支付单号(交易号)',
+  `pay_way` tinyint(4) DEFAULT NULL COMMENT '付款方式 0 未知, 1 微信支付, 2,支付宝, 3 网银支付',
+  `pay_time` datetime DEFAULT NULL COMMENT '订单支付时间',
+
   `goods_num` varchar(40) DEFAULT NULL COMMENT '商品编码',
   `goods_name` varchar(80) DEFAULT NULL COMMENT '商品名称',
-  `sale_price` decimal(20,4) DEFAULT '0.0000' COMMENT '销售价格',
-  `discount_price` decimal(20,4) DEFAULT '0.0000' COMMENT '折后价格',
-  `stock` bigint(20) DEFAULT '0' COMMENT '库存',
-  `sale_count` bigint(20) DEFAULT '0' COMMENT '销量',
-  `status` int(11) DEFAULT '1' COMMENT '状态  1:正常, 0:下架',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10011 DEFAULT CHARSET=utf8 COMMENT='商品信息表';
-
-
--- 增加默认数据
-INSERT INTO `pn_order` VALUES (10001, '10001', '苹果X', 100, 1000, 10, 10, 1);
+  `price` decimal(20,2) DEFAULT '0.0000' COMMENT '拍下价格',
+  `pay_price` decimal(20,2) DEFAULT '0.0000' COMMENT '支付价格',
+  `amount` bigint(20) DEFAULT '0' COMMENT '商品数量',
+  `buyer_name` varchar(80) DEFAULT NULL COMMENT '收货人',
+  `buyer_address` varchar(80) DEFAULT NULL COMMENT '收货地址',
+  `buyer_phone` varchar(80) DEFAULT NULL COMMENT '手机',
+  `create_id` bigint(20) DEFAULT NULL COMMENT '订单创建账号ID',
+  `create_time` datetime DEFAULT NULL COMMENT '订单创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '订单最后修改时间',
+  `status` int(11) DEFAULT '0' COMMENT '订单状态 0:未付款，1：已付款，2：已发货，3：已完成，4：取消订单，5：支付失败',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单信息表';
 
