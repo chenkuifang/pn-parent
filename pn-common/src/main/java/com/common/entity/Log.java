cbeae83d659e470bafcde2c84d297e5aa2439d3b
chenkuifang <314287696@qq.com>
2018-03-29 17:32:50 +0800

    update

---
+++ pn-common/src/main/java/com/common/entity/Log.java
@@ -0,0 +1,31 @@
+package com.common.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 系统日志实体类
+ *
+ * @author QuiFar
+ **/
+@Getter
+@Setter
+@ToString
+public class Log implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private Integer userId;
+    private String userName;
+    private String operation;
+    private String method;
+    private String params;
+    private String ip;
+    private Date createTime;
+}
+
---
+++ pn-common/src/main/java/com/common/entity/Menu.java
@@ -0,0 +1,38 @@
+
+package com.common.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author QuiFar
+ * @version V1.0
+ * @Description: 系统菜单实体类
+ * @date 2017年11月24日 下午20:53:00
+ */
+@Getter
+@Setter
+@ToString
+public class Menu implements Serializable {
+
+	private static final long serialVersionUID = -4716109663079532955L;
+
+	private Integer menuId; /* menu_id */
+	private Integer parentId; /* 父菜单ID，一级菜单为0 */
+	private String name; /* 菜单名称 */
+	private String url; /* 菜单URL */
+	private Integer type; /* 类型 0：目录 1：菜单 2：按钮 */
+	private String icon; /* 菜单图标 */
+	private Integer orderNum; /* 排序 */
+	private Date createTime; /* 创建时间 */
+	private Date updateTime; /* 修改时间 */
+	private Integer status; /* 状态 1：正常，0：暂停 */
+
+	// 角色菜单设置用到，其他没用
+	private String isChecked;/* 该菜单是否勾选 */
+
+}
