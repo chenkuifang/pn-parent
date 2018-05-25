02034a3831507be214b80fa83d77d3d2913204ab
chenkuifang <314287696@qq.com>
2018-05-25 17:18:54 +0800

    优化

--- pn-order/src/main/java/com/example/order/controller/OrderController.java
+++ pn-order/src/main/java/com/example/order/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.example.order.controller;
 
 import com.common.constant.Constants;
 import com.common.entity.Order;
-import com.example.order.MqProducer;
 import com.example.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +28,8 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    @Autowired
-    private MqProducer mqProducer;
-
     @GetMapping("/{id}")
-    public Order getOrder(@PathVariable("id") Integer id) {
+    public Order get(@PathVariable("id") Integer id) {
         return orderService.get(id);
     }
 
@@ -64,43 +60,6 @@ public class OrderController {
         order.setCreateTime(new Date());
         order.setStatus(0);
         return orderService.add(order);
-    }
-
-    @GetMapping("/add-order/{userId}/{goodsId}")
-    public void AddOrder(@PathVariable("userId") Integer userId, @PathVariable("goodsId") Integer goodsId) {
-        log.info("创建订单开始------");
-        String orderSid;
-        try {
-            orderSid = orderService.getNewOrderSid();
-        } catch (Exception e) {
-            log.info("创建订单失败。原因：创建订单编码出错");
-            return;
-        }
-
-        Order order = new Order();
-        order.setOrderSid(orderSid);
-        order.setPaySid("xxxxxxxx1");
-        order.setPayWay(1);
-        order.setGoodsId(goodsId);
-        order.setGoodsName("苹果X");
-        order.setPrice(new BigDecimal(100));
-        order.setAmount(1);
-        order.setCreateId(userId);
-        order.setCreateTime(new Date());
-        order.setStatus(0);
-
-        int flag = orderService.add(order);
-
-        // 创建失败
-        if (flag == 0) {
-            return;
-        }
-
-        // 发布消息
-        mqProducer.send("createOrder", order);
-
-        // 调用支付接口
-
     }
 
 }
---
+++ pn-order/src/main/java/com/example/order/service/OrderService.java
@@ -0,0 +1,85 @@
+package com.example.order.service;
+
+import com.common.entity.Order;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 用户服务层接口
+ * 
+ * @author QuiFar
+ * @version V1.0
+ */
+public interface OrderService {
+	/**
+	 * 根据主键删除单个对象
+	 *
+	 * @param id 主键
+	 * @return
+	 */
+	Integer remove(Integer id);
+
+	/**
+	 * 根据主键数据批量删除用户
+	 *
+	 * @param id 主键
+	 * @return
+	 */
+	Integer removeBatch(String[] ids);
+
+	/**
+	 * 根据主键ID更新单个对象
+	 *
+	 * @param order 需要更新的对象
+	 * @return
+	 */
+	Integer update(Order order);
+
+	/**
+	 * 新增
+	 *
+	 * @param order
+	 * @return
+	 */
+	Integer add(Order order);
+
+	/**
+	 * 根据主键获取单个对象
+	 *
+	 * @param id
+	 * @return
+	 */
+	Order get(Integer id);
+
+	/**
+	 * 创建订单SID
+	 *
+	 * @return
+	 */
+	String getNewOrderSid() throws Exception;
+
+	/**
+	 * 根据条件获取列表
+	 *
+	 * @param params
+	 * @return
+	 */
+	List<Order> list(Map<String, Object> params);
+
+	/**
+	 * 根据条件获取用户列表(分页)
+	 *
+	 * @param 1.过滤条件、2.分页参数必须包含page,limit
+	 * @return
+	 */
+	List<Order> listPage(Map<String, Object> params);
+
+	/**
+	 * 根据条件获取用户列表总行数(一般提供分页使用)
+	 *
+	 * @param params
+	 * @return
+	 */
+	Integer countPage(Map<String, Object> params);
+}
