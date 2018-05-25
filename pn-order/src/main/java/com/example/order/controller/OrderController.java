c07662d60c1661e8d76940e7159122933bc1f65e
chenkuifang <314287696@qq.com>
2018-05-25 17:32:42 +0800

    修改包

--- pn-order/src/main/java/com/example/order/controller/OrderController.java
+++ pn-order/src/main/java/com/example/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.example.order.controller;
 
 import com.common.constant.Constants;
 import com.common.entity.Order;
+import com.example.mq.MqProducer;
 import com.example.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,8 @@ import java.util.List;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private MqProducer mqProducer;
 
     @GetMapping("/{id}")
     public Order get(@PathVariable("id") Integer id) {
@@ -60,6 +63,41 @@ public class OrderController {
         order.setCreateTime(new Date());
         order.setStatus(0);
         return orderService.add(order);
+    }
+
+    @GetMapping("add-order/{userId}/{goodsId}")
+    public void addOrder(@PathVariable("userId") Integer userId, @PathVariable("goodsId") Integer goodsId) {
+
+        log.info("创建订单开始-----------");
+        String orderSid;
+        try {
+            orderSid = orderService.getNewOrderSid();
+        } catch (Exception e) {
+            log.info("创建订单失败，原因：创建订单编码失败");
+            return;
+        }
+
+        Order order = new Order();
+        order.setOrderSid(orderSid);
+        order.setPaySid("xxxxxxxx1");
+        order.setPayWay(1);
+        order.setGoodsId(goodsId);
+        order.setGoodsName("苹果X");
+        order.setPrice(new BigDecimal(100));
+        order.setAmount(1);
+        order.setCreateId(userId);
+        order.setCreateTime(new Date());
+        order.setStatus(0);
+        /*int flag = orderService.add(order);
+
+        if (flag == 0) {
+            return;
+        }*/
+
+        // 发送MQ
+        mqProducer.send("createOrder", order);
+
+
     }
 
 }
